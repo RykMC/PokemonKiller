777edecc39dfd09521e09aa61c7584d0e9d81ba6
unknown <rykmc@hotmail.deRykMC>
2025-06-23 17:18:01 +0200

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,8 @@
 // src/api/axios.js
 import axios from "axios";
 
-const API_URL = import.meta.env.REACT_APP_API_URL;
-
 const api = axios.create({
-   baseURL: API_URL || "http://localhost:8000/api",
+   baseURL: "https://project9-pokemon-battlegame.onrender.com/",
   timeout: 5000,
 });
 
