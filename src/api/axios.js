41615100e5384fd721fe746d209f86b4d2bdc32b
unknown <rykmc@hotmail.deRykMC>
2025-06-23 13:45:36 +0200

    axios2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,10 @@
 // src/api/axios.js
 import axios from "axios";
 
+const API_URL = import.meta.env.REACT_APP_API_URL;
+
 const api = axios.create({
-   baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api",
+   baseURL: API_URL || "http://localhost:8000/api",
   timeout: 5000,
 });
 
